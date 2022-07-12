fce99c5dd9669e6183a8326bc0ed3f751b0df3b6
Bui Van Thai <buithaild230899@gmail.com>
2022-07-13 00:46:12 +0700

    use database

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,6 +1,21 @@
+import connection from '../configs/connectDB';
 
 let getHomePage = (req, res) => {
-    res.render('test/index.ejs')
+    //simple query
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users` ',
+        function (err, results, fields) {
+            results.map((row) => {
+                data.push({
+                    id: row.id,
+                    email: row.email,
+                    firstName: row.firstName,
+                    lastName: row.lastName
+                })
+            });
+            res.render('test/index.ejs', { dataUser: JSON.stringify(data) })
+        })
 }
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
 import initWebRouter from "./route/web";
+// import connection from "./configs/connectDB";
 
 require('dotenv').config();
 
