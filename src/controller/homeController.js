1a758ba5a215de3f99ca7c98be4f7e7cb94e1cc4
Bui Van Thai <buithaild230899@gmail.com>
2022-07-13 11:49:38 +0700

    CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -22,6 +22,27 @@ let createNewUser = async (req, res) => {
     return res.redirect('/');// quay lai trang home
 }
 
+let deleteUser = async (req, res) => {
+    let userId = req.body.userId;
+    await pool.execute('delete from users where id = ?', [userId])
+    return res.redirect('/');// quay lai trang home
+}
+
+let getEditPage = async (req, res) => {
+    let id = req.params.id;
+    let [user] = await pool.execute('Select * from users where id = ? ', [id])
+    return res.render('update.ejs', { dataUser: user[0] }); // x <- y
+}
+
+let postUpdateUser = async (req, res) => {
+    let { firstName, lastName, email, address, id } = req.body;
+
+    await pool.execute('update users set firstName= ?, lastName = ? , email = ? , address= ? where id = ?',
+        [firstName, lastName, email, address, id]);
+
+    return res.redirect('/');
+}
+
 module.exports = {
-    getHomePage, getDetailPage, createNewUser
+    getHomePage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser
 }
